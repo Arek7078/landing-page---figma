56ccb58379b0ea7994dc28ad3b2902b5adc488b8
Arek908 <arekkasz07@gmail.com>
2025-06-10 07:18:25 +0100

    js and css updated

--- script/script.js
+++ script/script.js
@@ -1,26 +1,31 @@
 const nav = document.getElementById('navigation');
 const socials = document.getElementById('socials');
 const burger = document.getElementById('burger');
+const burgerContainer = document.getElementById('burger_container');
 const navLinks = document.getElementById('nav-links');
 const header = document.getElementById('header');
 const logo = document.getElementById('logo');
+const menuLinks = document.querySelectorAll('#nav-links a');
+const toggleElements = [logo, header, nav, navLinks, socials, burgerContainer];
 
 burger.addEventListener('click', function(){
-    logo.classList.toggle('active');
-    header.classList.toggle('active');
-    nav.classList.toggle('active');
-    navLinks.classList.toggle('active');
-    socials.classList.toggle('active');
+    toggleElements.forEach(element => {
+        element.classList.toggle('active');
+        
+    })
 }
 );
-navLinks.addEventListener('click', function(){
-    logo.classList.remove('active');
-    header.classList.remove('active');
-    nav.classList.remove('active');
-    navLinks.classList.remove('active');
-    socials.classList.remove('active');
-}
-);
+menuLinks.forEach(link => {
+    link.addEventListener('click',function(){
+        toggleElements.forEach(element => {
+            
+            element.classList.remove('active');
+            
+        })
+    })
+})
+
+
 
 
     
